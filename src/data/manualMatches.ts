7783cbdeffc15205309e9f626eb35328a6a1a55f
sandipbixta <xandipbixta@gmail.com>
2025-07-14 19:48:12 +1000

    Update manualMatches.ts

--- src/data/manualMatches.ts
+++ src/data/manualMatches.ts
@@ -50,7 +50,7 @@ export const manualMatches: ManualMatch[] = [
         quality: "HD"
       },     
     ],
-    visible: true,
+    visible: false,
     image: "https://i.imgur.com/rXT699O.jpeg",
     seo: {
       keywords: "FIFA Club WorldCup live stream, FiFA Club Worldcup watch online, Fifa club wprldcup free stream",
