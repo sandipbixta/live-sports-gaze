93bab077917881fab7d1c0ec4a32b04abdf15808
sandipbixta <xandipbixta@gmail.com>
2025-08-16 16:58:06 +1000

    Update manualMatches.ts

--- src/data/manualMatches.ts
+++ src/data/manualMatches.ts
@@ -50,7 +50,7 @@ export const manualMatches: ManualMatch[] = [
         quality: "HD"
       },     
     ],
-    visible: false,
+    visible: true,
     image: "",
     seo: {
       keywords: "FIFA Club WorldCup live stream, FiFA Club Worldcup watch online, Fifa club wprldcup free stream",
@@ -99,7 +99,7 @@ export const manualMatches: ManualMatch[] = [
         quality: "HD"
       },     
     ],
-    visible: false,
+    visible: true,
     image: "https://i.imgur.com/ZDK0tgZ.jpeg",
     seo: {
       keywords: "UEFA women euro 2025 live stream, UEFA watch online,euro women 2025 free stream",
@@ -147,7 +147,7 @@ export const manualMatches: ManualMatch[] = [
         quality: "HD"
       },     
     ],
-    visible: false,
+    visible: true,
     image: "https://i.imgur.com/2ttt7yh.jpeg",
     seo: {
       keywords: "Los Angeles FC vs Esperance Tunis live stream, Los Angeles FC Esperance Tunis watch online, Los Angeles FC vs Esperance Tunis free stream",
@@ -195,7 +195,7 @@ export const manualMatches: ManualMatch[] = [
         quality: "HD"
       },     
     ],
-    visible: false,
+    visible: true,
     image: "https://i.imgur.com/zZ4nHho.jpeg",
     seo: {
       keywords: "Bayern Munich vs Boca Juniors live stream, Bayern Munich Boca Juniors watch online, Bayern Munich vs Boca Juniors free stream",
@@ -219,7 +219,7 @@ export const manualMatches: ManualMatch[] = [
         quality: "HD"
       }
     ],
-    visible: false,
+    visible: true,
     seo: {
       keywords: "hidden match live stream",
       description: "Hidden match for testing purposes",
