5156407e216147d1a1225a71ea1c768dd9c4e969
Damitv <xandipbixta@gmail.com>
2025-11-22 22:13:54 +1100

    Update manualMatches.ts

--- src/data/manualMatches.ts
+++ src/data/manualMatches.ts
@@ -26,7 +26,7 @@ export const manualMatches: ManualMatch[] = [
         quality: "HD"
       }
     ],
-    visible: true,
+    visible: false,
     image: "",
     seo: {
       keywords: "Kathmandu vs Sudurpaschim cricket live stream",
